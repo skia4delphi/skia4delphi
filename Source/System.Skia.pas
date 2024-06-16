291b20a0bfe039ee07ab73e17e813e4019d7f602
Vinícius Felipe Botelho Barbosa <viniciusfbb2@gmail.com>
2024-06-16 12:22:42 +0100

    [Library] Remove _DELPHI_CONST for now to avoid issues prior RAD 12.1
    Co-Authored-By: Paulo César Botelho Barbosa <16469061+paulocesarbot@users.noreply.github.com>

--- Source/System.Skia.pas
+++ Source/System.Skia.pas
@@ -13418,15 +13418,15 @@ end;
 {$HPPEMIT END '    using ::System::Skia::TSkVertexMode;'}
 {$HPPEMIT END '    using ::System::Skia::TSkVertices;'}
 {$HPPEMIT END '    typedef TSkEncodedImageFormat (__fastcall *TExtensionToEncodedImageFormatFunc)(const ::System::UnicodeString AValue);'}
-{$HPPEMIT END '    static _DELPHI_CONST TSkFontSlant SkFontSlantRegular = ::System::Skia::SkFontSlantRegular;'}
-{$HPPEMIT END '    static _DELPHI_CONST TSkFontWeight SkFontWeightDemiBold = ::System::Skia::SkFontWeightDemiBold;'}
-{$HPPEMIT END '    static _DELPHI_CONST TSkFontWeight SkFontWeightHairline = ::System::Skia::SkFontWeightHairline;'}
-{$HPPEMIT END '    static _DELPHI_CONST TSkFontWeight SkFontWeightHeavy = ::System::Skia::SkFontWeightHeavy;'}
-{$HPPEMIT END '    static _DELPHI_CONST TSkFontWeight SkFontWeightRegular = ::System::Skia::SkFontWeightRegular;'}
-{$HPPEMIT END '    static _DELPHI_CONST TSkFontWeight SkFontWeightUltraBlack = ::System::Skia::SkFontWeightUltraBlack;'}
-{$HPPEMIT END '    static _DELPHI_CONST TSkFontWeight SkFontWeightUltraBold = ::System::Skia::SkFontWeightUltraBold;'}
-{$HPPEMIT END '    static _DELPHI_CONST TSkFontWeight SkFontWeightUltraLight = ::System::Skia::SkFontWeightUltraLight;'}
-{$HPPEMIT END '    static _DELPHI_CONST TSkFontWidth SkFontWidthRegular = ::System::Skia::SkFontWidthRegular;'}
+{$HPPEMIT END '    static const TSkFontSlant SkFontSlantRegular = ::System::Skia::SkFontSlantRegular;'}
+{$HPPEMIT END '    static const TSkFontWeight SkFontWeightDemiBold = ::System::Skia::SkFontWeightDemiBold;'}
+{$HPPEMIT END '    static const TSkFontWeight SkFontWeightHairline = ::System::Skia::SkFontWeightHairline;'}
+{$HPPEMIT END '    static const TSkFontWeight SkFontWeightHeavy = ::System::Skia::SkFontWeightHeavy;'}
+{$HPPEMIT END '    static const TSkFontWeight SkFontWeightRegular = ::System::Skia::SkFontWeightRegular;'}
+{$HPPEMIT END '    static const TSkFontWeight SkFontWeightUltraBlack = ::System::Skia::SkFontWeightUltraBlack;'}
+{$HPPEMIT END '    static const TSkFontWeight SkFontWeightUltraBold = ::System::Skia::SkFontWeightUltraBold;'}
+{$HPPEMIT END '    static const TSkFontWeight SkFontWeightUltraLight = ::System::Skia::SkFontWeightUltraLight;'}
+{$HPPEMIT END '    static const TSkFontWidth SkFontWidthRegular = ::System::Skia::SkFontWidthRegular;'}
 {$HPPEMIT END '    static ::System::StaticArray<int, 23>& SkBytesPerPixel = ::System::Skia::SkBytesPerPixel;'}
 {$HPPEMIT END '    static TSkColorMatrix& SkColorMatrixIdentity = ::System::Skia::SkColorMatrixIdentity;'}
 {$HPPEMIT END '    static TSkColorSpaceTransferFunction& SkColorSpaceTransferFunctionHLG = ::System::Skia::SkColorSpaceTransferFunctionHLG;'}
