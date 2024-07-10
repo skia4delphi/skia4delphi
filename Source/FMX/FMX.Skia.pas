11d6d65cc440b350abc92ba9e61253b8f7b422b1
Vinícius Felipe Botelho Barbosa <viniciusfbb2@gmail.com>
2024-07-10 16:15:54 +0100

    [Controls] Fix oblique fonts on macOS and iOS
    Co-Authored-By: Paulo César Botelho Barbosa <16469061+paulocesarbot@users.noreply.github.com>

--- Source/FMX/FMX.Skia.pas
+++ Source/FMX/FMX.Skia.pas
@@ -1422,7 +1422,9 @@ type
   {$ENDIF}
 
 const
-  SkFontSlant: array[TFontSlant] of TSkFontSlant = (TSkFontSlant.Upright, TSkFontSlant.Italic, TSkFontSlant.Oblique);
+  SkFontSlant : array[TFontSlant] of TSkFontSlant = (TSkFontSlant.Upright, TSkFontSlant.Italic,
+    // SkParagraph does not support oblique fonts on macOS on m107
+    {$IFDEF MACOS}TSkFontSlant.Italic{$ELSE}TSkFontSlant.Oblique{$ENDIF});
   SkFontWeightValue: array[TFontWeight] of Integer = (100, 200, 300, 350, 400, 500, 600, 700, 800, 900, 950);
   SkFontWidthValue: array[TFontStretch] of Integer = (1, 2, 3, 4, 5, 6, 7, 8, 9);
 
