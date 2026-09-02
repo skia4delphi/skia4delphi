391adff37f14b87705b1dd5e3e3326a113de7d97
Vinícius Felipe Botelho Barbosa <viniciusfbb@users.noreply.github.com>
2026-09-02 11:13:53 -0300

    [Samples] Fix chess background on VCL demo.
    Co-Authored-By: Paulo César Botelho Barbosa <16469061+paulocesarbot@users.noreply.github.com>

--- Samples/Demo/VCL/Source/Base/Sample.Form.Base.Viewer.pas
+++ Samples/Demo/VCL/Source/Base/Sample.Form.Base.Viewer.pas
@@ -604,8 +604,11 @@ begin
         begin
           for I := 0 to LScrollBox.ControlCount-1 do
           begin
-            LRect := LScrollBox.Controls[I].BoundsRect;
-            ExcludeClipRect(ADC, LRect.Left, LRect.Top, LRect.Right, LRect.Bottom);
+            if LScrollBox.Controls[I] is TSkCustomAnimatedControl then
+            begin
+              LRect := LScrollBox.Controls[I].BoundsRect;
+              ExcludeClipRect(ADC, LRect.Left, LRect.Top, LRect.Right, LRect.Bottom);
+            end;
           end;
           BitBlt(ADC, 0, 0, LScrollBox.Width, LScrollBox.Height, FChessBitmap.Canvas.Handle, 0, 0, SRCCOPY);
           SelectClipRgn(ADC, HRGN(nil));
